f0bae9c7c41c83c0e0703fe6538184546f8b7090
kevin Esteban Joya Aparicio <kjoya@INTELIBPO.local>
2025-08-26 10:01:45 -0500

    Actualizar main.py con nuevos cambios

--- main.py
+++ main.py
@@ -18,35 +18,35 @@ productos = [{"nombre":"camisa", "color":"azul"}]
 def ver_productos():
     return jsonify(productos),200 #codigo http  "OK"
 
+
 #servicio POST (insertar datos)
 @app.route('/productos', methods = ['POST'])
 def agregar_productos():
     data = request.get_json()
-    if not data or "nombre" not in data:
-        return jsonify({"error": "El producto debe tener un nombre"})
+    if not data or "nombre" not in data or "color" not in data: 
+        return jsonify({"error": "El producto debe tener un nombre"}), 400 #datos invalidos "bad request"
     
     productos.append(data)   #guarda los datos en la lista 
-    return jsonify(data)
+    return jsonify(data), 201 #creado 
 
 #servicio PUT (actualizar datos)
-@app.route('/productos/<int:inbdice>', methods = ['PUT'])
+@app.route('/productos/<int:indice>', methods = ['PUT'])
 def actualizar(indice):
     if indice < 0 or indice >= len(productos):
-        return jsonify({"error al actualizar"})
+        return jsonify({"error al actualizar"}), 404 #not found "no encontrado"
     data = request.get_json()  #recibir el json del postman
-    productos[indice].update(data)
-    return jsonify (productos[indice])
+    productos[indice].update(data) 
+    return jsonify (productos[indice]), 200 #ok
 
 #servicio DELETE (eliminar)
 @app.route('/productos/<int:indice>', methods = ['DELETE'])
 def eliminar_producto(indice):
     if indice < 0 or indice >= len(productos):
-        return jsonify({"indice desconocido"})
+        return jsonify({"indice desconocido"}), 404 #not found
     eliminado = productos.pop(indice)  # quitar el producto de la lista
     return jsonify({
         "mensaje": "se eliminó el producto",
-        "producto": eliminado
-        })
+        "producto": eliminado}), 200 #ok
 
 if __name__ == "__main__":       #ejecutar el archivo directamente
     app.run(debug=True)         #iniciar servidor (modo debug: autorecarga y errores)
